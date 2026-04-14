687433dd47e199335f019bb775786c64a6df03f0
bletadoci <bleta.doci@student.uni-pr.edu>
2026-04-14 15:27:13 +0200

    Added receive method and more
    Added receive method on client side

--- TCPClient/Program.cs
+++ TCPClient/Program.cs
@@ -11,9 +11,11 @@ class Program
 
     static void Main()
     {
+        Console.WriteLine("- TCP Client -");
         Console.Write("Server IP: ");
         string ip = Console.ReadLine()!;
 
+        Console.WriteLine("Connecting to " + ip + ":5050...");
         client = new TcpClient();
         client.Connect(ip, 5050);
 
@@ -26,11 +28,12 @@ class Program
 
         while (true)
         {
-            Console.WriteLine("\nCommands:");
+            Console.WriteLine("\n--- Available Commands ---");
             Console.WriteLine("1. read|file.txt");
             Console.WriteLine("2. write|file.txt|text");
             Console.WriteLine("3. list");
             Console.WriteLine("4. exec|something");
+            Console.WriteLine("--------------------------");
 
             string msg = Console.ReadLine()!;
             Send(msg);
@@ -43,7 +46,35 @@ class Program
         byte[] data = Encoding.UTF8.GetBytes(msg);
         stream.Write(data, 0, data.Length);
     }
-    
+    static void Receive()
+    {
+        byte[] buffer = new byte[2048];
+
+        while (true)
+        {
+            try
+            {
+                if (stream == null) break;
+                int bytes = stream.Read(buffer, 0, buffer.Length);
+                string msg = Encoding.UTF8.GetString(buffer, 0, bytes);
+
+                if (msg.StartsWith("ROLE:"))
+                {
+                    role = msg.Split(':')[1];
+                    Console.WriteLine("Your role: " + role);
+                }
+                else
+                {
+                    Console.WriteLine("SERVER: " + msg);
+                }
+            }
+            catch
+            {
+                break;
+            }
+        }
+    }
+
 
 }
 
