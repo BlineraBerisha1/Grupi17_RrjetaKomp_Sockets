4f74364b64eb1646b115ca470605450a0ad8cb24
blertalatifi <blerta.latifi4@student.uni-pr.edu>
2026-04-14 11:14:13 +0200

    Implement TCP client functionality with command handling

--- TCPClient/Program.cs
+++ TCPClient/Program.cs
@@ -1,6 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
-app.MapGet("/", () => "Hello World!");
+class Program
+{
+    static TcpClient? client;
+    static NetworkStream? stream;
+    static string? role;
 
-app.Run();
+    static void Main()
+    {
+        Console.Write("Server IP: ");
+        string ip = Console.ReadLine()!;
+
+        client = new TcpClient();
+        client.Connect(ip, 5050);
+
+        stream = client.GetStream();
+
+        Thread t = new Thread(Receive);
+        t.Start();
+
+        Console.WriteLine("Connected to server!");
+
+        while (true)
+        {
+            Console.WriteLine("\nCommands:");
+            Console.WriteLine("1. read|file.txt");
+            Console.WriteLine("2. write|file.txt|text");
+            Console.WriteLine("3. list");
+            Console.WriteLine("4. exec|something");
+
+            string msg = Console.ReadLine()!;
+            Send(msg);
+        }
+    }
--- TCPServer/obj/Debug/net8.0/TCPServer.AssemblyInfo.cs
+++ TCPServer/obj/Debug/net8.0/TCPServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCPServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2077648e234d3c72e56d1ebf97868f99b9a1490")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4758fabb778172e19afee60cd206e1808966ea5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCPServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCPServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
